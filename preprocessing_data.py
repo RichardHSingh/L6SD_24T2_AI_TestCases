0a6cc0884503b361836b2d2f222c8b2e0eef563f
Richard Singh <110203976+RichardHSingh@users.noreply.github.com>
2024-06-23 15:48:08 +1200

    Add files via upload
    optmising the old code so that it can better serve its purpose during pytests. Added in MinMaxScaler as well

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -6,6 +6,7 @@
 # ===================== IMPORTNG LIBRARIES =====================
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 
 
@@ -14,32 +15,46 @@ import numpy as np
 df_cpd = pd.read_csv("Car_Purchasing_Data.csv")
 
 
-def cpd_preprocessing_input_data(df_cpd):
+def cpd_preprocessing_data(df_cpd):
+    #definging the appropriate columns for intended use
     irrelevant_columns = ['Customer Name','Customer e-mail','Country', 'Car Purchase Amount']
     input_used_columns = ['Gender', 'Age', 'Annual Salary', 'Credit Card Debt', 'Net Worth']
+    output_column = ['Car Purchase Amount']   
+
+    # dropping irrelevant column X and extracting the output column Y
+    X = df_cpd.drop(irrelevant_columns, axis = 1)
+    Y = df_cpd[output_column]
+
+    # scaling the dropped columns
+    x_scaler = MinMaxScaler()
+    X_scaled = x_scaler.fit_transform(X)
+
+    # scaling hte output column
+    y_scaler = MinMaxScaler()
+    y_reshape = Y.values.reshape(-1,1)
+    Y_scaled = y_scaler.fit_transform(y_reshape)  
+
+    return X_scaled, Y_scaled, x_scaler, y_scaler
+
+
     
-    dropped_columns = [
-        col for col in df_cpd.columns if col in irrelevant_columns
-        ]
     
-    remainder_columns = [
-        col for col in df_cpd.columns if col not in dropped_columns
-        ]
-    
-    return df_cpd[remainder_columns], dropped_columns, remainder_columns
 
 
 
-def cpd_preprocessing_output_data(df_cpd):
-    irrelevant_columns = ['Customer Name', 'Customer e-mail', 'Country', 'Gender', 'Age', 'Annual Salary', 'Credit Card Debt', 'Net Worth']
-    output_used_columns = ['Car Purchase Amount']
+# ===================== ORIGINAL CAR PURCHASE FUNCTION ====================
+# def cpd_preprocessing_data(df_cpd):
+#     irrelevant_columns = ['Customer Name','Customer e-mail','Country', 'Car Purchase Amount']
+#     input_used_columns = ['Gender', 'Age', 'Annual Salary', 'Credit Card Debt', 'Net Worth']
     
-    dropped_columns = [
-        col for col in df_cpd.columns if col in irrelevant_columns
-        ]
+#     dropped_columns = [
+#         col for col in df_cpd.columns if col in irrelevant_columns
+#         ]
     
-    remainder_columns = [
-        col for col in df_cpd.columns if col not in dropped_columns
-        ]
+#     remainder_columns = [
+#         col for col in df_cpd.columns if col not in dropped_columns
+#         ]
     
-    return df_cpd[remainder_columns], dropped_columns, remainder_columns
+#     return df_cpd[remainder_columns], dropped_columns, remainder_columns
+
+# # scale and shape the data --> x scale and y scale and shape it .fit_transform
